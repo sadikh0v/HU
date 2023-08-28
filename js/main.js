0efe36d3f36c8c40387159f754449637329c601b
sadikh0v <sadixovnazimm1@gmail.com>
2023-08-28 19:40:54 +0400

    WIP

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/* HUReacts Theme Scripts */
+
+(function ($) {
+    "use strict";
+
+    $(window).on("load", function () {
+        $("body").addClass("loaded");
+    });
+
+    /*=========================================================================
+      Sticky Header
+  =========================================================================*/
+
+    $(function () {
+        var header = $("#header"),
+            yOffset = 0,
+            triggerPoint = 80;
+        $(window).on("scroll", function () {
+            yOffset = $(window).scrollTop();
+
+            if (yOffset >= triggerPoint) {
+                header.addClass("navbar-fixed-top");
+            } else {
+                header.removeClass("navbar-fixed-top");
+            }
+        });
+    });
+
+    /*=========================================================================
+          Mobile Menu
+  =========================================================================*/
+
+    $(".menu-wrap ul.nav").slicknav({
+        prependTo: ".header-section .navbar",
+        label: "",
+        allowParentLinks: true,
+    });
+
+    /*=========================================================================
+      Features Swipe
+  =========================================================================*/
+
+
+    /*=========================================================================
+      Initialize smoothscroll plugin
+  =========================================================================*/
+
+    smoothScroll.init({
+        offset: 60,
+    });
+
+    /*=========================================================================
+      Scroll To Top
+  =========================================================================*/
+
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 100) {
+            $("#scroll-to-top").fadeIn();
+        } else {
+            $("#scroll-to-top").fadeOut();
+        }
+    });
+})(jQuery);
